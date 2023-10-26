3d06053709784e178fdb7a3a6c2a44874260e47f
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-10-26 11:24:57 -0300

    BASE DE DATOSSS

--- ONIET2023app/models.py
+++ ONIET2023app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Registro(models.Model):
+    Registro = models.IntegerField(primary_key=True)
+    Empresa = models.TextField(max_length=15)
+    Mes = models.IntegerField()
+    ProduccionTotal = models.IntegerField()
+    CantidaPiezasConFallas = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Empresa} Mes: {self.Mes}"
