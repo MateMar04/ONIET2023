b7e417f1f77b6e21cc0dfebb180deb0538966885
Mateo Marchisone <mateo.marchisone@gmail.com>
2023-10-26 12:30:32 -0300

    Added missing columns

--- ONIET2023app/views.py
+++ ONIET2023app/views.py
@@ -1,26 +1,15 @@
-from ONIET2023app.models import Registro
 from django.db import connection
 from django.http import JsonResponse
-
-
-def get_registros_por_empresa(empresa):
-    registros = Registro.objects.filter(Empresa=empresa)
-
-    return registros
-
-
-def get_empresas():
-    empresas = Registro.objects.values('Empresa').distinct()
-    print(empresas)
-    return empresas
 
 
 def analisis_view(request):
     with connection.cursor() as cursor:
         cursor.execute('select Empresa, '
-                       '(SUM(ProduccionTotal) - SUM(CantidaPiezasConFallas))                                as piezas_ok, '
+                       'SUM(ProduccionTotal), '
+                       '(SUM(ProduccionTotal) - SUM(CantidaPiezasConFallas))                                as piezas_ok,'
+                       'SUM(CantidaPiezasConFallas), '
                        '(SUM(ProduccionTotal) - SUM(CantidaPiezasConFallas)) * 100.0 / SUM(ProduccionTotal) as "%piezas_ok", '
-                       '(SUM(CantidaPiezasConFallas)) * 100.0 / SUM(ProduccionTotal) as "%piezas_error" '
+                       '(SUM(CantidaPiezasConFallas)) * 100.0 / SUM(ProduccionTotal)                        as "%piezas_error" '
                        'from ONIET2023app_registro '
                        'group by Empresa')
         row = cursor.fetchall()
